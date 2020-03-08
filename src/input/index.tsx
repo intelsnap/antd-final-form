6047e1efa9fc9d22402938f21fa148db088cc6c0
Arif Hanif <arif.hanif3@gmail.com>
2020-03-08 16:23:29 -0400

    add indication of error to input

--- src/input/index.tsx
+++ src/input/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Input, Typography } from 'antd';
+import { ClosedCircleFilled } from '@ant-design/icons';
 import { FieldRenderProps } from 'react-final-form';
 
 const { Text } = Typography;
@@ -7,9 +8,10 @@ const { Text } = Typography;
 type Props = FieldRenderProps<string>;
 
 const AntdInput: React.FC<Props> = ({ input, meta, ...rest }: Props) => {
+  const isError = meta.error && meta.touched;
   return (
     <div>
-      <Input {...input} {...rest} />
+      <Input {...input} {...rest} suffix={isError && <CloseCircleFilled />} />
       {meta.error && meta.touched && <Text type='danger'>{meta.error}</Text>}
     </div>
   );
