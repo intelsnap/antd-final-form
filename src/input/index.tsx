3017856aa4f662d6b7353cf5883efa7e0221ad10
Arif Hanif <arif.hanif3@gmail.com>
2020-03-08 15:45:12 -0400

    Update index.tsx

--- src/input/index.tsx
+++ src/input/index.tsx
@@ -4,12 +4,12 @@ import { Input, Typography } from 'antd';
 const { Text } = Typography;
 
 const AntdInput = ({ input, meta, ...rest }: any): React.ReactElement => {
-	return (
-		<div>
-			<Input {...input} {...rest} />
-			{meta.error && meta.touched && <Text type="danger">{meta.error}</Text>}
-		</div>
-	);
+  return (
+    <div>
+      <Input {...input} {...rest} />
+      {meta.error && meta.touched && <Text type='danger'>{meta.error}</Text>}
+    </div>
+  );
 };
 export { AntdInput };
 export default AntdInput;
