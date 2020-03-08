7f9a4ac4c3aa469294c0202363f8ad17255edbd7
Arif Hanif <arif.hanif3@gmail.com>
2020-03-07 23:16:29 -0500

    fix pathing issues

--- src/input/index.tsx
+++ src/input/index.tsx
@@ -1,6 +1,43 @@
 import * as React from 'react'
 import { Input, Typography } from 'antd'
-import { FieldRenderProps } from '../types'
+
+export type FieldInputProps = {
+  name: string
+  onBlur: React.FocusEventHandler<HTMLInputElement>
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  onFocus: React.FocusEventHandler<HTMLInputElement>
+  value: any
+  type?: string
+  checked?: boolean
+  multiple?: boolean
+}
+
+export type FieldMetaProps = {
+  active?: boolean
+  data?: Object
+  dirty?: boolean
+  dirtySinceLastSubmit?: boolean
+  error?: any
+  initial?: any
+  invalid?: boolean
+  length?: number
+  modified?: boolean
+  pristine?: boolean
+  submitError?: any
+  submitFailed?: boolean
+  submitSucceeded?: boolean
+  submitting?: boolean
+  touched?: boolean
+  valid?: boolean
+  validating?: boolean
+  visited?: boolean
+}
+export type FieldRenderProps = {
+  input: FieldInputProps
+  meta: FieldMetaProps
+  isReadOnly: boolean
+}
+
 
 const { Text } = Typography
 
